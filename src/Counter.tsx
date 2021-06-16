f4d1418ac0416a0c7dc5c8f88665bdadcd674d85
Shin Tako <k1x.heart@icloud.com>
2021-06-17 05:18:56 +0900

    useReducer

---
+++ src/Counter.tsx
@@ -0,0 +1,37 @@
+import React, {useEffect, useRef, useState } from 'react';
+
+const Counter: React.FC<{}> = () => {
+  const [value, setValue] = useState<number>(0);
+
+  const increment = () => {
+    setValue((prevState) => prevState + 1);
+  }
+  const decrement = () => {
+    setValue((prevState) => prevState - 1);
+  }
+
+  const renderTimes = useRef<number>(0);
+  useEffect(() => {
+    renderTimes.current = renderTimes.current + 1;
+  });
+
+  const ref = useRef<HTMLInputElement>(null!);
+  const focusInput = () => {
+    ref.current.focus();
+  };
+
+  return (
+  <div>
+    <div>value: { value }</div>
+    <button onClick={ increment }>+1</button>
+    <button onClick={ decrement }>-1</button>
+    <div>This component was re-rendered {renderTimes.current}</div>
+    <input ref={ ref } type="test" />
+    <button onClick={ focusInput }>click Me!</button>
+
+  </div>
+  )
+};
+
+export default Counter;
+
